5be19e61521f492d48417784505d1aa1ce18b7d1
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-04-04 16:29:29 +0900

    도커컴포즈 배포 테스트 (파이어베이스 빈 제거 후 테스트)

--- src/main/java/com/example/miniproject/config/FirebaseConfig.java
+++ src/main/java/com/example/miniproject/config/FirebaseConfig.java
@@ -4,11 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 
-@Configuration
+//@Configuration
 public class FirebaseConfig {
 
     @PostConstruct
